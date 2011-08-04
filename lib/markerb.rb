1104a701b2974ea1043ce08b5fa0ea874acce3b9
Roberto Vasquez Angel <robotex@robotex.de>
2011-08-04 14:33:02 +0200

    Attempt to fix escaped strings

--- lib/markerb.rb
+++ lib/markerb.rb
@@ -14,7 +14,7 @@ module Markerb
     def call(template)
       compiled_source = erb_handler.call(template)
       if template.formats.include?(:html)
-        "Redcarpet.new(begin;#{compiled_source};end, *Markerb.processing_options).to_html".html_safe
+        "Redcarpet.new(begin;#{compiled_source};end, *Markerb.processing_options).to_html.html_safe"
       else
         compiled_source
       end
