ead2b620992f2210c06fc19eda7ac0c0c55b789e
Roberto Vasquez Angel <robotex@robotex.de>
2011-12-04 20:24:25 +0100

    Added default options

--- lib/markerb.rb
+++ lib/markerb.rb
@@ -16,7 +16,8 @@ module Markerb
       if template.formats.include?(:html)
         # "Redcarpet.new(begin;#{compiled_source};end, *Markerb.processing_options).to_html.html_safe"
         # Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :tables => true, *Markerb.processing_options).render(compiled_source).html_safe
-        "Redcarpet::Markdown.new(Redcarpet::Render::HTML, *Markerb.processing_options).render(begin;#{compiled_source};end).html_safe"
+        # "Redcarpet::Markdown.new(Redcarpet::Render::HTML, *Markerb.processing_options).render(begin;#{compiled_source};end).html_safe"
+        "Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :tables => true).render(begin;#{compiled_source};end).html_safe"
       else
         compiled_source
       end
--- lib/markerb/version.rb
+++ lib/markerb/version.rb
@@ -1,3 +1,3 @@
 module Markerb
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
